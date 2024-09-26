f35e1dfc298d6221244642b16523baae9f0ce300
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-26 10:51:05 +0200

    feat: Add common tags

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 locals {
   WESTEUROPE = "westeurope"
+  tags_common = {
+    environment = "training"
+    client      = "generali"
+  }
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "generali-example"
   location = local.WESTEUROPE
+  tags     = local.tags_common
 }
