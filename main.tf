249a007bee7dd1ff6e122a1c7dafcc7d5c5fa192
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-26 11:20:18 +0200

    feat: Create example storage account with container

--- main.tf
+++ main.tf
@@ -16,3 +16,30 @@ resource "azurerm_resource_group" "example" {
   tags       = local.tags_common
   managed_by = data.azurerm_resource_group.root.id
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "generaliexampletraining"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "example"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.example.name
+}
+
+output "primary_access_key" {
+  value     = azurerm_storage_account.example.primary_access_key
+  sensitive = true
+}
