5dfb20253fdab5a23b3c7729ccb233f2cc2f6d4e
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-26 15:19:58 +0200

    feat: Use AKS module for current cluster and create new one

--- aks.tf
+++ aks.tf
@@ -1,51 +1,21 @@
-locals {
-  aks_name               = "generali-aks-example"
-  aks_location           = "northeurope"
-  aks_kubernetes_version = "1.28"
-  aks_vm_size            = "Standard_B2s"
-  aks_node_count         = 1
+module "aks--generali-aks-example" {
+  source = "./modules/aks"
+
+  name               = "generali-aks-example"
+  location           = "northeurope"
+  kubernetes_version = "1.28"
+  vm_size            = "Standard_B2s"
+  node_count         = 1
+  tags_common        = local.tags_common
 }
 
-resource "azurerm_resource_group" "aks-example" {
-  name     = local.aks_name
-  location = local.aks_location
-}
+module "aks--example" {
+  source = "./modules/aks"
 
-
-resource "azurerm_kubernetes_cluster" "example" {
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
-  }
-  name                              = local.aks_name
-  location                          = azurerm_resource_group.aks-example.location
-  resource_group_name               = azurerm_resource_group.aks-example.name
-  dns_prefix                        = local.aks_name
-  kubernetes_version                = local.aks_kubernetes_version
-  role_based_access_control_enabled = true
-  tags                              = local.tags_common
-
-  default_node_pool {
-    name       = substr(replace(local.aks_name, "-", ""), 0, 12)
-    node_count = local.aks_node_count
-    vm_size    = local.aks_vm_size
-
-    upgrade_settings {
-      drain_timeout_in_minutes      = 0
-      max_surge                     = "10%"
-      node_soak_duration_in_minutes = 0
-    }
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin    = "azure"
-    service_cidr      = "10.43.0.0/16"
-    dns_service_ip    = "10.43.0.10"
-    load_balancer_sku = "standard"
-  }
+  name               = "example"
+  location           = "northeurope"
+  kubernetes_version = "1.28"
+  vm_size            = "Standard_B2s"
+  node_count         = 1
+  tags_common        = local.tags_common
 }
