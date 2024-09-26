377d0eee78c87d250b172baa8b704a472b2f0575
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-26 15:51:16 +0200

    feat(aks): Add cluster foo using module from modules repo

--- aks.tf
+++ aks.tf
@@ -19,3 +19,14 @@ module "aks--example" {
   node_count         = 1
   tags_common        = local.tags_common
 }
+
+module "foo_aks" {
+  source = "git::https://github.com/sika-training-examples/2024-09-26-generali-terraform-modules-example.git//modules/aks"
+
+  name               = "foo"
+  location           = "northeurope"
+  kubernetes_version = "1.28"
+  vm_size            = "Standard_B2s"
+  node_count         = 1
+  tags_common        = local.tags_common
+}
